f00b9c6a87532ede1e3847c53a6dc16b154fbd21
Anze Skulj <anze.skulj@comsensus.eu>
2025-04-08 10:46:17 +0200

    changes

--- lib/mix/tasks/my.installer.ex
+++ lib/mix/tasks/my.installer.ex
@@ -21,7 +21,7 @@ defmodule Mix.Tasks.My.Installer do
   end
 
   defp copy_templates(app_path) do
-    app_module = get_app_module_name(app_path)
+    app_module = get_app_module_name()
 
     assigns = [app_module: app_module]
 
@@ -53,15 +53,11 @@ defmodule Mix.Tasks.My.Installer do
     end
   end
 
-  defp get_app_module_name(app_path) do
-    mix_file = Path.join(app_path, "mix.exs")
-    content = File.read!(mix_file)
+  defp get_app_module_name do
+    Mix.Project.get!()
+      |> Module.split()
+      |> Enum.at(0)
 
-    # Match `mod: {YourAppName.Application, _}`
-    case Regex.run(~r/mod:\s*\{\s*([A-Za-z0-9_.]+)\s*,/, content) do
-      [_, app_module] -> app_module
-      _ -> raise "Could not detect app module name"
-    end
   end
 
   defp render_template(template_path, assigns) do
